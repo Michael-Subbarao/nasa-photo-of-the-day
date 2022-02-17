7c5f49224248795edde85c84c178fc6ac04db8af
Michael-Subbarao <53024176+Michael-Subbarao@users.noreply.github.com>
2022-02-17 18:55:56 -0500

    Installed styled-components. Added some placeholder stylings

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledPhoto = styled.div`
+    width: 60%;
+    display: flex;
+    justify-content: space-between;
+`
+
 
 const Photo = (props) =>{
     return(
-        <div className = "photoContainer">
+        <StyledPhoto className = "photoContainer">
             <h1>{props.photo.title}</h1>
             <p>{props.photo.date}</p>         
             <img src ={props.photo.hdurl} alt={props.photo.title}/>
             <p>{props.photo.explanation}</p>
-        </div>
+        </StyledPhoto>
     )
 }
 
