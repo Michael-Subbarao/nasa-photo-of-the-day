e7b5fab592b02acc04c72da697f7e526e2624fb3
Michael-Subbarao <53024176+Michael-Subbarao@users.noreply.github.com>
2022-02-17 19:03:48 -0500

    Fixed basic styleings

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledPhoto = styled.div`
-    width: 60%;
+const StyledPhotoContainer = styled.div`
+    width: 100%;
     display: flex;
     justify-content: space-between;
+    flex-direction: column;
 `
 
+const StyledImg = styled.img`
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 30%;
+`
 
 const Photo = (props) =>{
     return(
-        <StyledPhoto className = "photoContainer">
+        <StyledPhotoContainer className = "photoContainer">
             <h1>{props.photo.title}</h1>
             <p>{props.photo.date}</p>         
-            <img src ={props.photo.hdurl} alt={props.photo.title}/>
+            <StyledImg src ={props.photo.hdurl} alt={props.photo.title}/>
             <p>{props.photo.explanation}</p>
-        </StyledPhoto>
+        </StyledPhotoContainer>
     )
 }
 
